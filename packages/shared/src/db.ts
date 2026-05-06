0b76cc4e089e382227ca92b2e6e25366840e1b8f
anonlegionoke <anonlegionoke@protonmail.com>
2026-05-06 10:50:14 +0530

    feat: implement database connection pool and environment configuration in shared package

---
+++ packages/shared/src/db.ts
@@ -0,0 +1,25 @@
+// DB Pool
+import { Pool } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config({ path: '../../../.env' });
+
+if (!process.env.DATABASE_URL) {
+    throw new Error('FATAL: DATABASE_URL environment variable is missing.');
+}
+
+export const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000
+});
+
+
+pool.on('error', (err, client) => {
+    console.error('Unexpected error on idle database client', err);
+});
+
+export const query = (text: string, params?: any[]) => {
+    return pool.query(text, params);
+}
